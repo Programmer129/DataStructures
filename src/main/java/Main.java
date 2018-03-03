5037defddaf2d709ee227f4857a7862c2c2355c2
ioseb <ioseb.khutsishvili@introlab-systems.com>
2018-03-03 17:06:55 +0400

    week 3 day 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 import org.omg.CORBA.INTERNAL;
 import speedTest.TreeTime;
 import tree.BinarySearchTree;
+import tree.SegmentTree;
 import tree.ThreadSafe;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
 
+        int arr [] = {1,2,3,4};
+        SegmentTree segmentTree = new SegmentTree(arr);
+        segmentTree.printSegmentTree();
 
     }
 }
--- src/main/java/tree/SegmentTree.java
+++ src/main/java/tree/SegmentTree.java
@@ -1,55 +1,120 @@
 package tree;
 
-import java.util.Arrays;
-
 public class SegmentTree {
 
     private int [] segmentTree;
+    private int [] initialArray;
     private static final int MAXVALUE = Integer.MAX_VALUE;
+    private static final int MINVALUE = Integer.MIN_VALUE;
 
     public SegmentTree(int [] initialArray){
         int low = 0;
         int high = initialArray.length - 1;
+        this.initialArray = initialArray;
         this.segmentTree = new int[segmentTreeLength(initialArray.length)];
-        Arrays.fill(this.segmentTree,MAXVALUE);
-        constructSegmentTree(initialArray,low,high,0);
+        constructSegmentTreeForXorAndOrOperations(initialArray,low,high,0,2);
     }
 
     private int segmentTreeLength(int initialArrayLength){
         if((initialArrayLength & (initialArrayLength - 1)) == 0){
             return 2*initialArrayLength - 1;
         }
-        int result = 0;
+        int result = 1;
         while(result<=initialArrayLength){
             result <<= 1;
         }
-        return result - 1;
+        return 2*result - 1;
     }
 
-    private void constructSegmentTree(int [] initialArray,int lowIndex,int highIndex,int position){ }
-
-    private int rangeMinQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+    private void constructSegmentTree(int [] initialArray,int lowIndex,int highIndex,int position){
+        if(lowIndex == highIndex){
+            this.segmentTree[position] = initialArray[lowIndex];
+            return;
+        }
+        int middle = (lowIndex + highIndex)/2;
+        constructSegmentTree(initialArray,lowIndex,middle,2*position + 1);
+        constructSegmentTree(initialArray,middle + 1,highIndex,2*position + 2);
+        this.segmentTree[position] = segmentTree[2*position+1]*segmentTree[2*position+2];
     }
 
-    private int rangeMaxQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+
+    private void constructSegmentTreeForXorAndOrOperations(int [] initialArray,int lowIndex,int highIndex,int position,
+                                                           int heightOfSegmentTree){
+        if(lowIndex == highIndex){
+            this.segmentTree[position] = initialArray[lowIndex];
+            return;
+        }
+        int middle = (lowIndex + highIndex)/2;
+        constructSegmentTreeForXorAndOrOperations(initialArray,lowIndex,middle,2*position+1,heightOfSegmentTree-1);
+        constructSegmentTreeForXorAndOrOperations(initialArray,middle+1,highIndex,2*position+2,heightOfSegmentTree-1);
+        if(heightOfSegmentTree %2 ==1){
+            segmentTree[position] = segmentTree[2*position+1] | segmentTree[2*position+2];
+        }
+        else{
+            segmentTree[position] = segmentTree[2*position+1] ^ segmentTree[2*position+2];
+        }
     }
 
-    private int rangeMinSumQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+    public int queryMaximum(int lowIndex,int highIndex){
+        return rangeMaxQuery(0,initialArray.length-1,lowIndex,highIndex,0);
     }
 
-    private int rangeMaxSumQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+    private int rangeMaxQuery(int lowIndex,int highIndex,int requiredLowIndex,int requeredHighIndex,int position){
+        if(requiredLowIndex<=lowIndex && highIndex<=requeredHighIndex){
+            return segmentTree[position];
+        }
+        else
+            if(requeredHighIndex<lowIndex || requiredLowIndex>highIndex){
+                return MINVALUE;
+            }
+            else{
+                int middle = (lowIndex + highIndex)/2;
+                return Math.max(rangeMaxQuery(lowIndex,middle,requiredLowIndex,requeredHighIndex,2*position+1),
+                        rangeMaxQuery(middle+1,highIndex,requiredLowIndex,requeredHighIndex,2*position+2));
+            }
     }
 
-    private int rangeMinProductQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+    private int rangeProductQuery(int lowIndex,int highIndex,int requiredLowIndex,int requeredHighIndex,int position){
+        if(requiredLowIndex<=lowIndex && highIndex<=requeredHighIndex){
+            return segmentTree[position];
+        }
+        else
+        if(requeredHighIndex<lowIndex || requiredLowIndex>highIndex){
+            return 1;
+        }
+        else{
+            int middle = (lowIndex + highIndex)/2;
+            return rangeProductQuery(lowIndex,middle,requiredLowIndex,requeredHighIndex,2*position+1)*
+                    rangeProductQuery(middle+1,highIndex,requiredLowIndex,requeredHighIndex,2*position+2);
+        }
     }
 
-    private int rangeMaxProductQuery(int requiredLowIndex,int requeredHighIndex,int position){
-        return -1;
+    public int queryProduct(int requiredLowIndex,int requiredHighIndex){
+       return rangeProductQuery(0,initialArray.length-1,requiredLowIndex,requiredHighIndex,0);
+    }
+
+    private void updateSegmentTreeForProduct(int lowIndex,int highIndex,int changeableIndex,int valueToChange,int position){
+        if(changeableIndex<lowIndex || changeableIndex>highIndex){
+            return;
+        }
+        if(lowIndex == highIndex){
+            segmentTree[position] = valueToChange;
+            return;
+        }
+        int middle = (lowIndex + highIndex)/2;
+        updateSegmentTreeForProduct(lowIndex,middle,changeableIndex,valueToChange,2*position+1);
+        updateSegmentTreeForProduct(middle+1,highIndex,changeableIndex,valueToChange,2*position+2);
+        segmentTree[position] = segmentTree[2*position+1]*segmentTree[2*position+2];
+    }
+
+    public void updateSegmentTree(int changeableIndex,int valueToChange){
+        updateSegmentTreeForProduct(0,initialArray.length-1,changeableIndex,valueToChange,0);
+    }
+
+    public void printSegmentTree(){
+        for (int i = 0; i < this.segmentTree.length; i++) {
+            System.out.println(segmentTree[i]);
+        }
     }
 
 }
