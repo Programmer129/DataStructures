05fa82f4ef715249a508c9be2c95e760c41dbf3b
avto <avtobolashvili@mail.com>
2018-03-08 11:28:27 +0400

    week 4 day 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,37 @@
 import strings.Trie;
+import strings.TrieNode;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
 
-        try(Scanner in = new Scanner(System.in)) {
-            int n = in.nextInt();
-            Trie trie = new Trie();
-            for(int i=0;i<n;i++){
-                String s = in.next();
-                String s1 = in.next();
-                if(s.equals("add")){
-                    trie.insert(s1);
-                }
-                else{
-                    System.out.println(trie.numberOfPrefix(s1));
-                }
-            }
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
+//        try(Scanner in = new Scanner(System.in)) {
+//            int n = in.nextInt();
+//            Trie trie = new Trie();
+//            for(int i=0;i<n;i++){
+//                String s = in.next();
+//                String s1 = in.next();
+//                if(s.equals("add")){
+//                    trie.insert(s1);
+//                }
+//                else{
+//                    System.out.println(trie.numberOfPrefix(s1));
+//                }
+//            }
+//        }
+//        catch (Exception ex){
+//            ex.printStackTrace();
+//        }
 
+        TrieNode trieNode = new TrieNode();
+        TrieNode trieNode1 = new TrieNode();
+        trieNode.hashMap.put('a',new TrieNode());
+        trieNode1.hashMap.put('b',new TrieNode());
+        System.out.println(trieNode.equals(trieNode1));
+        System.out.println(trieNode.hashCode());
+        System.out.println(trieNode1.hashCode());
 
     }
 }
--- src/main/java/strings/TrieNode.java
+++ src/main/java/strings/TrieNode.java
@@ -1,9 +1,10 @@
 package strings;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
-class TrieNode {
+public class TrieNode {
 
     public Map<Character,TrieNode> hashMap;
     public boolean isLast;
@@ -13,4 +14,13 @@ class TrieNode {
         this.isLast = false;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        return this.isLast == ((TrieNode)o).isLast && this.hashMap.equals(((TrieNode)o).hashMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return hashMap.hashCode() + Boolean.hashCode(isLast);
+    }
 }
