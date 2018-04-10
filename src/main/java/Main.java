8e31a008685b989000d197974d70187cd163ad9c
programmer129 <avtobolashvili@mail.com>
2018-04-10 23:40:38 +0400

    week 8 day 4: empty

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,6 @@
-import grapth.Pair;
-import grapth.SimulateGraph;
-import grapth.weighted_graph.WeithedGraph;
-import grapth.weighted_graph.sssp.Dijkstra;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        WeithedGraph<Integer, Integer> graph = WeithedGraph.getInstance();
-
-        graph.setInitialSize(1000);
-
-        List<Pair<Integer,Pair<Integer,Integer>>> edges = SimulateGraph.generateTestCase();
-
-        for (Pair<Integer, Pair<Integer, Integer>> edge : edges) {
-            graph.addEdge(edge.getFirst() - 1,edge.getSecond().getFirst() - 1,edge.getSecond().getSecond());
-        }
-
-        Dijkstra<Integer, Integer> dijkstra = new Dijkstra<>(graph.getGraph());
-
-        dijkstra.SSSPDijkstra(0);
-
-        for(int i = 1; i< 1000; i++){
-            System.out.println("shortest path from 0 to "+i+": "+dijkstra.shortestPathTo(i));
-        }
 
     }
 }
